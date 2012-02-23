638d1e0e0380d53628f9fc9976b22d38d39b3533
jlechem <jlechem@83a70eb9-b268-4724-8314-bc2f95db9b19>
2012-02-23 18:36:14 +0000

    Adding loggging to application.

--- trunk/IPViewer/Logger.cpp
+++ trunk/IPViewer/Logger.cpp
@@ -3,10 +3,27 @@
 
 
 CLogger::CLogger(void)
+	: m_strMessageLogFileName(_T(""))
+	, m_strErrorLogFileName(_T(""))
 {
 }
 
 
 CLogger::~CLogger(void)
+{
+}
+
+
+void CLogger::LogError(CException* exception)
+{
+}
+
+
+void CLogger::LogError(CString error)
+{
+}
+
+
+void CLogger::Log(CString message)
 {
 }
