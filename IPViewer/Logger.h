80457c665b6d427dd327807815ca6fbfdf517072
Justin LeCheminant <jlechem@gmail.com>
2012-02-23 18:36:14 +0000

    Adding loggging to application.

--- IPViewer/Logger.h
+++ IPViewer/Logger.h
@@ -4,5 +4,11 @@ class CLogger
 public:
 	CLogger(void);
 	~CLogger(void);
+	void LogError(CException* exception);
+	void LogError(CString error);
+	void Log(CString message);
+private:
+	CString m_strMessageLogFileName;
+	CString m_strErrorLogFileName;
 };
 
