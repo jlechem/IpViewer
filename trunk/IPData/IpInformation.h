960ede89801a3490867fef9acb3e2b54734d66f1
jlechem <jlechem@83a70eb9-b268-4724-8314-bc2f95db9b19>
2012-07-14 02:04:25 +0000

    More changes to support showing all adapters

--- trunk/IPData/IpInformation.h
+++ trunk/IPData/IpInformation.h
@@ -1,18 +1,19 @@
 #pragma once
 
 #include "stdafx.h"
+#include <vector>
 
 #ifndef __AFXWIN_H__
 	#error "include 'stdafx.h' before including this file for PCH"
 #endif
+
+using namespace std;
 
 class DLLEXPORT CIpInformation
 {
 private:
 	CString m_strAdapterName;
 	CString m_strAdapterDescription;
-	CString m_strIpV4Address;
-	CString m_strIpV6Address;
 	CString m_strMAC;
 	CString m_strSubnet;
 	CString m_strDefaultGateway;
@@ -23,12 +24,13 @@ private:
 	BOOL m_isIpV4;
 	BOOL m_isIpV6;
 
+	vector<CString*>* m_pIpAddresses; 
+
 public:
 
 	CString GetAdapterName() { return m_strAdapterName; }
 	CString GetAdapterDescription() { return m_strAdapterDescription; }
-	CString GetIpV4Address() { return m_strIpV4Address; }
-	CString GetIpV6Address() { return m_strIpV6Address; }
+	vector<CString*>* GetIpAddresses() { return m_pIpAddresses; }
 	CString GetMac() { return m_strMAC; }
 	CString GetSubnet() { return m_strSubnet; }
 	CString GetDefaultGateway() { return m_strDefaultGateway; }
@@ -38,8 +40,8 @@ public:
 
 	void SetAdapterName( CString value ) { m_strAdapterName = value; }
 	void SetAdapterDescription( CString value ) { m_strAdapterDescription = value; }
-	void SetIpV4Address( CString value ) { m_strIpV4Address = value; }
-	void SetIpV6Address( CString value ) { m_strIpV6Address = value; }
+	void AddIpAddress( CString* value );
+	void ClearIpAddresses();
 	void SetMAC( CString value ) { m_strMAC = value; }
 	void SetSubnet( CString value ) { m_strSubnet = value; }
 	void SetDefaultGateway( CString value ) { m_strDefaultGateway = value; }
