5951067b3431dc819ec05151286d5c9e99391e68
Justin LeCheminant <jlechem@gmail.com>
2020-10-03 20:56:32 -0600

    Working on .net core version

--- IpViewerCore/Form1.Designer.cs
+++ IpViewerCore/Form1.Designer.cs
@@ -2,36 +2,10 @@
 {
     partial class Form1
     {
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label labelExternalAddress;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBoxAdapters;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem copyInternalIPAddressToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem copyExternalIPAddressToolStripMenuItem;
-        private System.Windows.Forms.Label labelAdapterStatus;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label labelMacAddress;
-        private System.Windows.Forms.Label labelSpeed;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label labelHostName;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.ToolStripMenuItem refreshIPInformationToolStripMenuItem;
-        private System.Windows.Forms.Label labelInternalAddress;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
@@ -67,34 +41,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.ClientSize = new System.Drawing.Size(586, 386);
-           
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.labelHostName = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.labelExternalAddress = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.labelInternalAddress = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.labelSpeed = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.labelMacAddress = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.labelAdapterStatus = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBoxAdapters = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.refreshIPInformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.copyInternalIPAddressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.copyExternalIPAddressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -106,173 +52,26 @@
             this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.labelHostName);
-            this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.labelExternalAddress);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "IP Information";
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-
-            // 
-            // labelHostName
-            // 
-            this.labelHostName.Name = "labelHostName";
-            // 
-            // label9
-            // 
-           
-            this.label9.Name = "label9";
-            // 
-            // labelExternalAddress
-            // 
-            this.labelExternalAddress.Name = "labelExternalAddress";
-            // 
-            // label2
-            // 
-            this.label2.Name = "label2";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.labelInternalAddress);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.labelSpeed);
-            this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Controls.Add(this.labelMacAddress);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.labelAdapterStatus);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.comboBoxAdapters);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Text = "Adapter Information";
-
-            // 
-            // labelInternalAddress
-            // 
-            this.labelInternalAddress.Name = "labelInternalAddress";
-            // 
-            // label5
-            // 
-            this.label5.Name = "label5";
-            // 
-            // labelSpeed
-            // 
-            this.labelSpeed.Name = "labelSpeed";
-            // 
-            // label7
-            // 
-            this.label7.Name = "label7";
-            // 
-            // labelMacAddress
-            // 
-            this.labelMacAddress.Name = "labelMacAddress";
-            // 
-            // label6
-            // 
-            this.label6.Name = "label6";
-            // 
-            // labelAdapterStatus
-            // 
-            this.labelAdapterStatus.Name = "labelAdapterStatus";
-            // 
-            // label4
-            // 
-            this.label4.Name = "label4";
-            // 
-            // comboBoxAdapters
-            // 
-            this.comboBoxAdapters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxAdapters.FormattingEnabled = true;
-            this.comboBoxAdapters.Name = "comboBoxAdapters";
-            // 
-            // label3
-            // 
-            this.label3.Name = "label3";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.editToolStripMenuItem,
-            this.helpToolStripMenuItem});
-            this.menuStrip1.Name = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.refreshIPInformationToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // refreshIPInformationToolStripMenuItem
-            // 
-            this.refreshIPInformationToolStripMenuItem.Name = "refreshIPInformationToolStripMenuItem";
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.settingsToolStripMenuItem,
-            this.toolStripSeparator1,
-            this.copyInternalIPAddressToolStripMenuItem,
-            this.copyExternalIPAddressToolStripMenuItem});
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-
-            // 
-            // settingsToolStripMenuItem
-            // 
-            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-           
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-           
-            // 
-            // copyInternalIPAddressToolStripMenuItem
-            // 
-            this.copyInternalIPAddressToolStripMenuItem.Name = "copyInternalIPAddressToolStripMenuItem";
-           
-            // 
-            // copyExternalIPAddressToolStripMenuItem
-            // 
-            this.copyExternalIPAddressToolStripMenuItem.Name = "copyExternalIPAddressToolStripMenuItem";
-            
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aboutToolStripMenuItem});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-           
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-          
-            // 
             // statusStrip1
-          //
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 364);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusStrip1.Size = new System.Drawing.Size(586, 22);
+            this.statusStrip1.TabIndex = 3;
             // 
             // notifyIcon1
             // 
-          
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             // 
             // contextMenuStrip1
@@ -286,73 +85,132 @@
             this.toolStripSeparator4,
             this.toolStripMenuItem2});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-           
+            this.contextMenuStrip1.Size = new System.Drawing.Size(68, 110);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-           
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(67, 22);
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
-           
+            this.toolStripSeparator3.Size = new System.Drawing.Size(64, 6);
             // 
             // settingsToolStripMenuItem1
             // 
             this.settingsToolStripMenuItem1.Name = "settingsToolStripMenuItem1";
-           
+            this.settingsToolStripMenuItem1.Size = new System.Drawing.Size(67, 22);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-          
+            this.toolStripSeparator2.Size = new System.Drawing.Size(64, 6);
             // 
             // copyExternalAddressToolStripMenuItem
             // 
             this.copyExternalAddressToolStripMenuItem.Name = "copyExternalAddressToolStripMenuItem";
-            
+            this.copyExternalAddressToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
             // 
             // toolStripSeparator4
             // 
             this.toolStripSeparator4.Name = "toolStripSeparator4";
-          
+            this.toolStripSeparator4.Size = new System.Drawing.Size(64, 6);
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-        
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(67, 22);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 3600000;
             // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemEdit,
+            this.toolStripMenuItemHelp});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(586, 24);
+            this.menuStrip2.TabIndex = 4;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItemFile.Text = "&File";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(162, 22);
+            this.toolStripMenuItem4.Text = "&Refresh IP Values";
+            // 
+            // toolStripMenuItemEdit
+            // 
+            this.toolStripMenuItemEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3});
+            this.toolStripMenuItemEdit.Name = "toolStripMenuItemEdit";
+            this.toolStripMenuItemEdit.Size = new System.Drawing.Size(39, 20);
+            this.toolStripMenuItemEdit.Text = "&Edit";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(116, 22);
+            this.toolStripMenuItem3.Text = "Settings";
+            // 
+            // toolStripMenuItemHelp
+            // 
+            this.toolStripMenuItemHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem5});
+            this.toolStripMenuItemHelp.Name = "toolStripMenuItemHelp";
+            this.toolStripMenuItemHelp.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItemHelp.Text = "&Help";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItem5.Text = "&About";
+            // 
             // Form1
             // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(586, 386);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip2;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Text = "IP Viewer 6.0";
             this.contextMenuStrip1.ResumeLayout(false);
-            this.Text = "IP Viewer 5.0";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemEdit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelp;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
     }
 }
 
